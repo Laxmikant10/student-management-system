67f3aeecf8d4d8a69b994c572e2a514c7e875dc3
lax <codewithlax@gmail.com>
2023-05-25 21:29:33 +0530

    Implementation Update Student Feature

--- src/main/java/com/lax/sms/controller/StudentController.java
+++ src/main/java/com/lax/sms/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
@@ -21,20 +22,49 @@ public class StudentController {
     @GetMapping("/students")
     public String listOfStudents(Model model) {
         model.addAttribute("students", studentService.getAllStudents());
+        //return view
         return "students";
     }
+
     @GetMapping("/students/new")
-    public String createStudentForm(Model model){
+    public String createStudentForm(Model model) {
 
         //create Student object to hold student form data
         Student student = new Student();
-        model.addAttribute("student",student);
+        model.addAttribute("student", student);
+        //return view
         return "create_student";
     }
 
     @PostMapping("/students")
-    public String saveStudent(@ModelAttribute("student") Student student){
+    public String saveStudent(@ModelAttribute("student") Student student) {
         studentService.saveStudent(student);
         return "redirect:/students";
+    }
+
+    @GetMapping("/students/edit/{id}")
+    public String editStudentForm(@PathVariable(name = "id") Long id, Model model) {
+        model.addAttribute("student", studentService.getStudentById(id));
+        //return view
+        return "edit_student";
+
+    }
+
+    @PostMapping("/students/{id}")
+    public String updateStudent(@PathVariable Long id,
+                                @ModelAttribute("student") Student student,
+                                Model model) {
+
+        //get student from DB by id
+        Student existingStudent = studentService.getStudentById(id);
+        existingStudent.setId(id);
+        existingStudent.setFirstName(student.getFirstName());
+        existingStudent.setLastName(student.getLastName());
+        existingStudent.setEmail(student.getEmail());
+
+        //save updated Student object
+        studentService.updateStudent(existingStudent);
+        return "redirect:/students";
+
     }
 }
